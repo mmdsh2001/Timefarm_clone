8d740262a1237d566b6c92f73cc1efffb30c1880
mmdsh2001 <140174940+mmdsh2001@users.noreply.github.com>
2025-01-11 02:03:26 +0330

    post data to server to validate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
 import './App.css';
-import {useEffect, useState} from "react"
+import {useEffect, useState} from "react";
+import dotenv from "dotenv";
 
 import Header from './components/header';
 import MainContent from './components/mainContent';
 import Menu from './components/menu';
 
+
+
+
 function App() {
   
+  dotenv.config();
+  const serverUrl = process.env.server_domain
   // setting some placeholders
   const fname = "john doe";
   const points = 0;
 
 
-
   const initData = window.Telegram.WebApp.initData;
+  if (initData){
 
-  const params = new URLSearchParams(initData);
-  const user = JSON.parse(params.get('user'));
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`${serverUrl}/api/validate`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(initData),
+          });
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {
+          console.error('There was a problem with the fetch operation:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  }
 
-  console.log(user); // { id: 123456789, first_name: "John", ... }
-  const items = Object.entries(user);
+  // const params = new URLSearchParams(initData);
+  // const user = JSON.parse(params.get('user'));
+
+  // console.log(user); // { id: 123456789, first_name: "John", ... }
+  // const items = Object.entries(user);
 
   return (
     <div className="App">
       <Header fname={fname} />
       <MainContent points={points} />
-      <p>{items}</p>
       <Menu />
     </div>
   );
